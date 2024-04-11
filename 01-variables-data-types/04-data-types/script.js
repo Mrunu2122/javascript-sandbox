adf70459b55a832eb11e47c1e625482120a8ee64
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-12 02:22:16 +0530

    js

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,37 @@
+// string
+const firstName = 'Mrunal';  
+           
+// Number 
+const age = 30;
+const temp = 98.9;
+
+// Boolean
+const hasKids= true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+
+// Referance types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+    name: 'mrunal',
+};
+
+function sayHello() {
+    console.log('Hello');
+}
+
+const output = sayHello;
+
+
+console.log(output, typeof output);
