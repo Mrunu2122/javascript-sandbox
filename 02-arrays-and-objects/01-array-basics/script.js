1918ba15abb57b972cc1bc55fd77ab68eda5816e
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-16 16:23:34 +0530

    js

---
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,28 @@
+// Array literal
+const numbers = [12, 45, 33, 29, 39];
+const mixed = [12, 'Hello', true, null];
+
+// Array Constructor
+
+const fruits = new Array('apple', 'grape', 'banana');
+
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+x = `My favorite fruit is a ${fruits[2]}`;
+
+x = numbers.length;
+
+fruits[1] = 'orange';
+
+// fruits.length = 2;
+
+fruits[3] = 'strawberry';
+
+fruits[fruits.length] = 'blueberry';
+fruits[fruits.length] = 'peach';
+
+x = fruits;
+
+console.log(x);
