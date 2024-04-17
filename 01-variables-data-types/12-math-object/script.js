d7c0acbc1ffe253c57ee6e81f18ebc2785cff6a3
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-17 17:34:13 +0530

    js

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,19 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2);
+
+x = Math.ceil(4, 2);
+
+x = Math.floor(4.9);
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5, 3);
+
+x = Math.max(4,5,3);
+
+console.log(x);
