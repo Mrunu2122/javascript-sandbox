1184342d327508324cb43fa9cd3e56d777aafbe5
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-17 17:56:05 +0530

    js

---
+++ 01-variables-data-types/13-number-challege/script.js
@@ -0,0 +1,29 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+// Addition
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+ 
+console.log(sum);
+
+// Substraction
+const sub = x - y;
+const subOutput = `${x} - ${y} = ${sub}`;
+console.log(subOutput);
+
+// Multiplication
+const multi = x * y;
+const multiOutput = `${x} * ${y} = ${multi}`;
+console.log(multiOutput);
+
+// Quotient
+const quot = x / y;
+const quotOutput = `${x} / ${y} = ${quot}`;
+console.log(quotOutput);
+
+// Reminder
+const rem = x % y;
+const remOutput = `${x} % ${y} = ${rem}`;
+console.log(remOutput);
