b9b8ced66a264169cf81c8370fd26177433ad31c
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-17 18:12:57 +0530

    js

---
+++ 01-variables-data-types/14-dates-and-times/script.js
@@ -0,0 +1,23 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2024, 3, 22, 12, 1, 0);
+
+d = new Date('2024-04-22');
+d = new Date('04/22/2024 12:00:00');
+d = new Date('2024-03-21');
+
+d = Date.now();
+
+d = new Date('03-21-2024 12:30:00');
+d = d.getTime();
+d = d.valueOf();
+
+d = new Date(183929383);
+
+d = Math.floor(Date.now() / 1000);
+
+console.log(d);
