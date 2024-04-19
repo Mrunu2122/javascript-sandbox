c971b9d3c6291d6a68db9d12fd4c773e8a83aba2
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-19 17:05:05 +0530

    js

--- 02-arrays-and-objects/02-array-methods/script.js
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -1,3 +1,19 @@
+let x;
+
 const arr = [34, 55, 95, 20, 15];
 
-console.log(arr);
+arr.push(100);
+
+arr.pop();
+
+arr.unshift(99);
+
+arr.shift();
+
+arr.reverse();
+
+x = arr.includes(200);
+
+x = arr.indexOf(15);
+
+console.log(x);
