4f7bb19a96ef63508974c1b901a7d5f628d07b5c
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-23 11:08:51 +0530

    js

--- 02-arrays-and-objects/04-array-challenge/script.js
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -2,17 +2,8 @@ let x;
 
 const arr = [1, 2, 3, 4, 5];
 
-x = Array.from('0123456');
+arr.reverse();
+arr.push(0);
+arr.unshift(6);
 
-const a = 0;
-const b = 1;
-const c = 2;
-const d = 3;
-const e = 4;
-const f = 5;
-const g = 6;
-
-x = Array.of(a, b, c, d, e, f, g);
-
-console.log(x, arr);
-
+console.log(arr);
