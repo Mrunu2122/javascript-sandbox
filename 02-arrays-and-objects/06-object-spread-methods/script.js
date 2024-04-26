e9224494e3dda75e948de5d63960d279a3adcbee
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-04-26 15:36:59 +0530

    js

--- 02-arrays-and-objects/06-object-spread-methods/script.js
+++ 02-arrays-and-objects/06-object-spread-methods/script.js
@@ -23,7 +23,18 @@ const obj1 = {a: 1, b: 2};
 const obj2 = {c: 3, d: 4};
 
 const obj3 = {...obj1, ...obj2};
+const obj4 = Object.assign({}, obj1, obj2);
 
-x = obj3;
+const todos = [
+    {id: 1, name: 'Buy milk'},
+    {id: 2, name: 'Pickup Me from work'},
+    {id: 3, name: 'Go to Dinner'},
+];
+
+x = todos[1].name;
+
+x = Object.keys(todo);
+
+x = Object.keys(todo).length;
 
 console.log(x);
